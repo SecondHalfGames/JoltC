6dd0991c5e60f873b07d6d34e07e8dbed552fe68
Lucien Greathouse <me@lpghatguy.com>
2024-04-17 01:08:18 -0400

    Add JPC_FactoryInit

--- JoltC/JoltC.cpp
+++ JoltC/JoltC.cpp
@@ -2,7 +2,12 @@
 
 #include "Jolt/Jolt.h"
 #include "Jolt/Core/Memory.h"
+#include "Jolt/Core/Factory.h"
 
 void JPC_RegisterDefaultAllocator() {
 	JPH::RegisterDefaultAllocator();
+}
+
+void JPC_FactoryInit() {
+	JPH::Factory::sInstance = new JPH::Factory();
 }
