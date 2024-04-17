cf44144e71784a0f06dbeed98b6bf6ee65725e2d
Lucien Greathouse <me@lpghatguy.com>
2024-04-17 01:26:28 -0400

    extern C?

--- JoltC/Functions.h
+++ JoltC/Functions.h
@@ -2,5 +2,13 @@
 
 #define JPC_API extern __declspec(dllexport)
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 JPC_API void JPC_RegisterDefaultAllocator();
 JPC_API void JPC_FactoryInit();
+
+#ifdef __cplusplus
+}
+#endif
